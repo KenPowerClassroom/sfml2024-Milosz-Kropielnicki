f4a67a0ce43105c8d72abe2d2799a185d9a1d882
U-Unknown\User <Unknown+User@CD-D417-C31.setu.ie>
2025-11-12 14:43:41 +0000

    Refactor: Renamed all variables to be self-explanatory

--- 16_SFML_Games/snake.cpp
+++ 16_SFML_Games/snake.cpp
@@ -2,29 +2,29 @@
 #include <time.h>
 using namespace sf;
 
-int N = 30, M = 20; // Dictates the amount of movement blocks
-int sz = 16;    // Sprite/Texture pixel size(?)
-int w = sz*N;   // width, sz(?) * N blocks
-int h = sz*M;   // height, sz(?) * M blocks
+int blockNumWidth = 30, blockNumHeight = 20; // Dictates the amount of movement blocks
+int blockSize = 16;    // Sprite/Texture pixel size
+int screenWidth = blockSize * blockNumWidth;   // screenWidth, blockSize * N blocks
+int screenHeight = blockSize * blockNumHeight;   // screenHeight, blockSize * M blocks
 
-int dir,num=4;  // The direction amount, I guess?
+int direction, num = 4;  // The direction amount, I guess?
 
 // The snake
 // x,y are movement directions
-// s is the actual snake object
+// snakeChar is the actual snake object
 // The array declaration dictates the max length of the snake
 struct Snake 
 {
     int x,y;
-}   s[100];
+}   snakeChar[100];
 
 // The Fruit
 // x,y are spawn locations
-// f is the actual fruit object
+// fruit is the actual fruit object
 struct Fruit
 {
     int x,y;
-}   f;
+}   fruit;
 
 void Tick()
  {
@@ -32,55 +32,55 @@ void Tick()
     // Somehow relates to the end point/tail of the snake
     for (int i = num; i > 0; --i)
     {
-        s[i].x = s[i-1].x; 
-        s[i].y = s[i-1].y;
+        snakeChar[i].x = snakeChar[i-1].x; 
+        snakeChar[i].y = snakeChar[i-1].y;
     }
 
     // Changes snake direction based on input
-    if (dir == 0)
+    if (direction == 0)
     {
-        s[0].y += 1;
+        snakeChar[0].y += 1;
     }
-    if (dir == 1)
+    if (direction == 1)
     {
-        s[0].x -= 1;
+        snakeChar[0].x -= 1;
     }
-    if (dir == 2)
+    if (direction == 2)
     {
-        s[0].x += 1;
+        snakeChar[0].x += 1;
     }
-    if (dir == 3)
+    if (direction == 3)
     {
-        s[0].y -= 1;
+        snakeChar[0].y -= 1;
     }
 
     // if snake head is in the same position as fruit
     // Increases num by 1
     // Consequently, increases the for loop count at the beginning of Tick()
     //Finally, randomizes fruit position
-    if ((s[0].x == f.x) && (s[0].y == f.y)) 
+    if ((snakeChar[0].x == fruit.x) && (snakeChar[0].y == fruit.y)) 
     {
         num++;
-        f.x = rand()%N;
-        f.y = rand()%M;
+        fruit.x = rand() % blockNumWidth;
+        fruit.y = rand() % blockNumHeight;
     }
 
     // Border wrapping checks
-    if (s[0].x > N)
+    if (snakeChar[0].x > blockNumWidth)
     {
-        s[0].x = 0;
+        snakeChar[0].x = 0;
     }
-    if (s[0].x < 0)
+    if (snakeChar[0].x < 0)
     {
-        s[0].x = N;
+        snakeChar[0].x = blockNumWidth;
     }
-    if (s[0].y > M)
+    if (snakeChar[0].y > blockNumWidth)
     {
-        s[0].y = 0;
+        snakeChar[0].y = 0;
     }
-    if (s[0].y < 0)
+    if (snakeChar[0].y < 0)
     {
-        s[0].y = M;
+        snakeChar[0].y = blockNumHeight;
     }
  
     // i = index
@@ -88,7 +88,7 @@ void Tick()
     // Consequently, for loop at the start is affected
     for (int i = 1; i < num; i++)
     {
-        if (s[0].x == s[i].x && s[0].y == s[i].y)
+        if (snakeChar[0].x == snakeChar[i].x && snakeChar[0].y == snakeChar[i].y)
         {
             num = i;
         }
@@ -99,29 +99,29 @@ int snake()
 {  
     srand(time(0));
 
-    // Renders the window using the width and height set at the top
-    RenderWindow window(VideoMode(w, h), "Snake Game!");
+    // Renders the window using the screenWidth and screenHeight set at the top
+    RenderWindow window(VideoMode(screenWidth, screenHeight), "Snake Game!");
 
-    // Creates Textures t1 and t2
-    // t1 is set to the white blocks (for empty squares)
-    // t2 us set to the red blocks (for snake/fruit)
-    Texture t1,t2;
-    t1.loadFromFile("images/snake/white.png");
-    t2.loadFromFile("images/snake/red.png");
+    // Creates Textures emptyTile and occupiedTile
+    // emptyTile is set to the white blocks (for empty squares)
+    // occupiedTile us set to the red blocks (for snake/fruit)
+    Texture emptyTileTexture, occupiedTileTexture;
+    emptyTileTexture.loadFromFile("images/snake/white.png");
+    occupiedTileTexture.loadFromFile("images/snake/red.png");
 
     // Creates Sprites sprite 1 and sprite 2
-    // sprite1 is set the white block texture
-    // sprite2 is set the red block texture
-    Sprite sprite1(t1);
-    Sprite sprite2(t2);
+    // emptyTileSprite is set the white block texture
+    // occupiedTileSprite is set the red block texture
+    Sprite emptyTileSprite(emptyTileTexture);
+    Sprite occupiedTileSprite(occupiedTileTexture);
 
     // Sets up a clock
     Clock clock;
     float timer=0, delay=0.1;
 
     // Base(?) fruit position
-    f.x=10;
-    f.y=10; 
+    fruit.x=10;
+    fruit.y=10; 
     
     while (window.isOpen())
     {
@@ -144,19 +144,19 @@ int snake()
         // Sets direction based on key press
         if (Keyboard::isKeyPressed(Keyboard::Left))
         {
-            dir = 1;
+            direction = 1;
         }
         if (Keyboard::isKeyPressed(Keyboard::Right))
         {
-            dir = 2;
+            direction = 2;
         }
         if (Keyboard::isKeyPressed(Keyboard::Up))
         {
-            dir = 3;
+            direction = 3;
         }
         if (Keyboard::isKeyPressed(Keyboard::Down))
         {
-            dir = 0;
+            direction = 0;
         }
 
         // If timer exceeds delay, reset the timer and call Tick()
@@ -171,23 +171,23 @@ int snake()
         
         // Draws the white (empty) boxes
         // Draws them based on the predetermined repeat values set into N and M at the top
-        for (int i = 0; i < N; i++)
+        for (int i = 0; i < blockNumWidth; i++)
         {
-            for (int j = 0; j < M; j++)
+            for (int j = 0; j < blockNumHeight; j++)
             {
-                sprite1.setPosition(i * sz, j * sz);    // Sets the sprite position based off of the texture size * index
-                window.draw(sprite1);
+                emptyTileSprite.setPosition(i * blockSize, j * blockSize);    // Sets the sprite position based off of the texture size * index
+                window.draw(emptyTileSprite);
             }
         }
 
         for (int i = 0; i < num; i++)
         {
-            sprite2.setPosition(s[i].x * sz, s[i].y * sz);  // Sets the snake sprite position based off of the texture size
-            window.draw(sprite2); 
+            occupiedTileSprite.setPosition(snakeChar[i].x * blockSize, snakeChar[i].y * blockSize);  // Sets the snake sprite position based off of the texture size
+            window.draw(occupiedTileSprite); 
         }
    
-        sprite2.setPosition(f.x * sz, f.y * sz);    // Draws fruit
-        window.draw(sprite2);    
+        occupiedTileSprite.setPosition(fruit.x * blockSize, fruit.y * blockSize);    // Draws fruit
+        window.draw(occupiedTileSprite);    
 
         window.display();
     }
